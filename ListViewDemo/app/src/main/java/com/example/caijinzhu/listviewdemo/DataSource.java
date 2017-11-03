fc03f793b892ff393a580acda22a99fde3a3a987
Alexiuce <alexiuce@qq.com>
2017-11-03 16:53:09 +0800

    datasource class

---
+++ ListViewDemo/app/src/main/java/com/example/caijinzhu/listviewdemo/DataSource.java
@@ -0,0 +1,45 @@
+package com.example.caijinzhu.listviewdemo;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+/**
+ * Created by caijinzhu on 2017/11/3.
+ */
+
+public class DataSource extends BaseAdapter{
+
+    @Override
+    public int getCount() {
+        return 40;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View cellView;
+        if (convertView != null){
+            cellView =  convertView;
+        }else {
+            cellView = View.inflate(parent.getContext(), R.layout.main_cell_item, null);
+        }
+        TextView titleView = (TextView) cellView.findViewById(R.id.tv_titleView);
+        titleView.setText("from xml view" + position);
+        return cellView;
+    }
+}
--- ListViewDemo/app/src/main/java/com/example/caijinzhu/listviewdemo/MainActivity.java
+++ ListViewDemo/app/src/main/java/com/example/caijinzhu/listviewdemo/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
  public class MainActivity extends AppCompatActivity  {
 
      private ListView lv;   // listView;
-     private View inflate;
+
 
 
      @Override
@@ -25,34 +25,4 @@ import android.widget.TextView;
 
      }
 
-    private class DataSource extends BaseAdapter{
-        @Override      // 返回条目数量
-        public int getCount() {
-            return 40;
-        }
-
-        @Override      // 返回position位置的对象
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override       // 返回position位置对应的id
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override     // 相当于iOS中的cellForRow ,返回每行的视图
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            if (convertView != null){
-                inflate =  convertView;
-            }else {
-                inflate = View.inflate(getApplicationContext(), R.layout.main_cell_item, null);
-            }
-            TextView titleView = (TextView) inflate.findViewById(R.id.tv_titleView);
-            titleView.setText("from xml view" + position);
-
-            return inflate;
-        }
-    }
  }

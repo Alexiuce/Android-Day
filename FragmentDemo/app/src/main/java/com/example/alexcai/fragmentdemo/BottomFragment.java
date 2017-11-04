b206f25add380685b7164def4f2b853741f1a42d
Alexiuce <alexiuce@qq.com>
2017-11-04 18:28:49 +0800

    programe add fragment to viewGroup

--- FragmentDemo/app/src/main/java/com/example/alexcai/fragmentdemo/BottomFragment.java
+++ FragmentDemo/app/src/main/java/com/example/alexcai/fragmentdemo/BottomFragment.java
@@ -1,9 +1,10 @@
 package com.example.alexcai.fragmentdemo;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,24 +13,24 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link TopFragment.OnFragmentInteractionListener} interface
+ * {@link BottomFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link TopFragment#newInstance} factory method to
+ * Use the {@link BottomFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TopFragment extends Fragment {
+public class BottomFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-//     TODO: Rename and change types of parameters
+    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    private OnBottomFragmentListener mListener;
 
-    public TopFragment() {
+    public BottomFragment() {
         // Required empty public constructor
     }
 
@@ -39,52 +40,51 @@ public class TopFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TopFragment.
+     * @return A new instance of fragment BottomFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TopFragment newInstance(String param1, String param2) {
-        TopFragment fragment = new TopFragment();
+    public static BottomFragment newInstance(String param1, String param2) {
+        BottomFragment fragment = new BottomFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        return inflater.inflate(R.layout.fragment_top,container,false);
+        return inflater.inflate(R.layout.fragment_bottom, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+            mListener.onBottomFragmentInteraction(uri);
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnBottomFragmentListener) {
+//            mListener = (OnBottomFragmentListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
 
     @Override
     public void onDetach() {
@@ -102,8 +102,8 @@ public class TopFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
+    public interface OnBottomFragmentListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onBottomFragmentInteraction(Uri uri);
     }
 }
--- FragmentDemo/app/src/main/java/com/example/alexcai/fragmentdemo/MainActivity.java
+++ FragmentDemo/app/src/main/java/com/example/alexcai/fragmentdemo/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.alexcai.fragmentdemo;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TopFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        View bottomLayout = findViewById(R.id.fl_bottomLayout);
+
+
+        FragmentManager fragmentManager = getFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fl_bottomLayout,new BottomFragment());
+
+        fragmentTransaction.commit();
+
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }

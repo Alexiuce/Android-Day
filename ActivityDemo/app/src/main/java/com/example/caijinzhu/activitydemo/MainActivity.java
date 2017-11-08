1059afc029404b45dbee7dea6fc6f4a55f08507f
Alexiuce <alexiuce@qq.com>
2017-11-08 16:33:30 +0800

    add jumt to SplashActivity

---
+++ ActivityDemo/app/src/main/java/com/example/caijinzhu/activitydemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.caijinzhu.activitydemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    // MARK: - 按钮点击事件处理
+    public void openNewActivity(View v){
+        Intent it = new Intent(this,SplashActivity.class);
+        
+        startActivity(it);
+    }
+}

95ed77a97291523faf030db3bb0a9433b0cbafad
Alexiuce <alexiuce@qq.com>
2017-12-14 16:20:27 +0800

    add update module

---
+++ UpdateApkDemo/app/src/main/java/com/joinf/www/updateapkdemo/ApkUpdateModule/ApkUpdateManager.java
@@ -0,0 +1,17 @@
+package com.joinf.www.updateapkdemo.ApkUpdateModule;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by caijinzhu on 2017/12/14.
+ * apk 版本更新管理模块
+ */
+
+public class ApkUpdateManager {
+
+    /** 显示更新提示UI*/
+    public static void showUpdateUI(AppCompatActivity activity){
+        
+
+    }
+}

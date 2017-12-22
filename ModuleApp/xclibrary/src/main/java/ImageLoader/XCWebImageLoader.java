f87ebbda7bae57bd351fc29591275ddfdd67517d
Alexiuce <alexiuce@qq.com>
2017-12-22 10:24:55 +0800

    add XCWebImageLoader

--- ModuleApp/xclibrary/src/main/java/ImageLoader/XCWebImageLoader.java
+++ ModuleApp/xclibrary/src/main/java/ImageLoader/XCWebImageLoader.java
@@ -1,8 +1,25 @@
 package ImageLoader;
 
+
+
 /**
  * Created by caijinzhu on 2017/12/22.
+ * Imageloader 工具类,用来加载网络图片
  */
 
 public class XCWebImageLoader {
+
+    private static final int THREAD_COUNT = 4;   // 线程数
+
+    private static final int PROPRITY = 2;      // 图片加载的优先级
+
+    private static final int DISK_CACHE_SIZE = 50 * 1024; // 图片缓存个数
+
+    private static final int CONNECT_TIME_OUT = 5 * 1000; // 超时时间
+
+
 }
+
+
+
+

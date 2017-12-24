d4fe64f4c86b37249183e8b6e3fbdfd03fbb2d7e
Alexiuce <alexiuce@qq.com>
2017-12-24 17:19:02 +0800

    add rotate animation

--- SmartCityDemo/app/src/main/java/com/alexcai/smartcitydemo/SplashActivity.java
+++ SmartCityDemo/app/src/main/java/com/alexcai/smartcitydemo/SplashActivity.java
@@ -2,6 +2,8 @@ package com.alexcai.smartcitydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_layout);
+
+        /**添加动画效果*/
+        beginAnimation();
+    }
+
+    /** 动画*/
+    private void beginAnimation() {
+        RotateAnimation rotateAnimation = new RotateAnimation(0,
+                360,
+                Animation.RELATIVE_TO_SELF,
+                0.5f,Animation.RELATIVE_TO_SELF,
+                0.5f);
+        rotateAnimation.setDuration(1000);  // 设置动画时间 1秒
+        rotateAnimation.setFillAfter(true); // 保持动画结束状态
+
     }
 }

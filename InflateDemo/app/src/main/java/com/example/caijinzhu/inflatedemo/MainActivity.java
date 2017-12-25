0c7be78873893ef9526f0cf6e33c9542bb738abd
Alexiuce <alexiuce@qq.com>
2017-12-25 16:58:38 +0800

    config ViewPager

--- InflateDemo/app/src/main/java/com/example/caijinzhu/inflatedemo/MainActivity.java
+++ InflateDemo/app/src/main/java/com/example/caijinzhu/inflatedemo/MainActivity.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout pointContrainer;
     private TextView descTextView;
     private ViewPager viewPage;
+    private int[] pictureIDArray;
+    private ImageView[] imageViews;
+    private ArrayList<ImageView> pictureArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +50,17 @@ public class MainActivity extends AppCompatActivity {
         pointContrainer = (LinearLayout) findViewById(R.id.layout_pointContainer);
         descTextView = (TextView)findViewById(R.id.textView_scrollDescription);
         viewPage = (ViewPager) findViewById(R.id.viewPager_scrollViewPager);
-        viewPage.setAdapter();
+        pictureIDArray = new int[]{R.mipmap.a,R.mipmap.b,R.mipmap.c,R.mipmap.d,R.mipmap.e};
+
+        pictureArray = new ArrayList<ImageView>();
+
+        ImageView imgView;
+        for (int i = 0; i < pictureIDArray.length; i++){
+            imgView = new ImageView(this);
+            imgView.setBackgroundResource(pictureIDArray[i]);
+            pictureArray.add(imgView);
+        }
+        viewPage.setAdapter(new ViewPagerAdapter());
     }
 
 
@@ -65,12 +81,36 @@ public class MainActivity extends AppCompatActivity {
     private class ViewPagerAdapter extends PagerAdapter {
         @Override
         public int getCount() {
-            return 0;
+            return pictureArray.size();
         }
 
+
+        /** 3. 判断是否需要复用
+         * */
         @Override
         public boolean isViewFromObject(View view, Object object) {
-            return false;
+            // 当滑动新条目又返回时,view是否可以被复用
+            // 返回判断规则
+            return view == object;
+        }
+
+        /** 1. 返回要显示的条目
+         * container : 就是ViewPager本身
+         * position: 条目的位置
+         * */
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView imageView =  pictureArray.get(position);
+            container.addView(imageView);
+            return imageView;
+        }
+
+        /**
+         *  2.销毁 */
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+
+            container.removeView((View)object);
         }
     }
 

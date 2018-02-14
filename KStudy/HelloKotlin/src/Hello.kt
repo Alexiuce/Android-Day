d469edbdc5e53ec65e2a7c93134a03bb60be850c
Alexiuce <alexiuce@qq.com>
2018-02-14 19:59:21 +0800

    update kotlin plugin new

--- KStudy/HelloKotlin/src/Hello.kt
+++ KStudy/HelloKotlin/src/Hello.kt
@@ -1,20 +1,6 @@
 /**
- * Created by caijinzhu on 2018/2/12.
+ * Created by Alexcai on 2018/2/14.
  */
-
-fun main(args:Array<String>){
-    println("hello kotlin")
-
-    val aByte:Byte = Byte.MIN_VALUE
-    val bByte:Byte = Byte.MAX_VALUE
-    println("a min" + aByte)
-    println("b max" + bByte)
-
-    val number1 = Math.pow(2.0,100.0)
-
-}
-
-
-fun test(){
-    println("test func run")
+fun main(args: Array<String>) {
+    println("hello demo")
 }
--- KStudy/HelloKotlin/src/TestClass.kt
+++
@@ -1,6 +0,0 @@
-/**
- * Created by caijinzhu on 2018/2/12.
- */
-fun main(args: Array<String>) {
-    println("test class")
-}

254cc25d2c89a290d303b4ea096c26028b55a1a6
Alexiuce <alexiuce@qq.com>
2018-03-09 16:43:50 +0800

    add enmu, varargs

---
+++ KStudy/04kotlin/src/main/kotlin/github/alexiuce/CmdParamaters.kt
@@ -0,0 +1,15 @@
+package github.alexiuce
+
+/**
+ * Created by caijinzhu on 2018/3/9.
+ */
+
+fun main(args: Array<String>) {
+
+//    args.map(::println)
+
+    args.map {
+        println(it)
+    }
+
+}

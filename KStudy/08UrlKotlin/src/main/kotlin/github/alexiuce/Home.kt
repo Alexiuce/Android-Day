13ecd41908f244f8d771cfac2b1a0b1131cd7f7b
Alexiuce <alexiuce@qq.com>
2018-03-19 12:33:26 +0800

    add list map range

--- KStudy/08UrlKotlin/src/main/kotlin/github/alexiuce/Home.kt
+++ KStudy/08UrlKotlin/src/main/kotlin/github/alexiuce/Home.kt
@@ -7,14 +7,22 @@ import java.net.URL
  */
 
 fun main(args: Array<String>) {
+    val list = listOf("crop","wheat","rice")
+    for (item in list){
+        println(item)
+    }
+    for ((i,e) in list.withIndex()){
+        println("index = $i element = $e")
+    }
 
+
+}
+
+fun demo1(){
     val url = "http://www.httpbin.org/get"
- 
+
     print("hello...")
     val result = URL(url).readText()
     println(result)
     print("good")
-
-
-
 }

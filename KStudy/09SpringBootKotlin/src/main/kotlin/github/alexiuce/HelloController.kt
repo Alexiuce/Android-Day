e91ecedbaf46ed42a191c00c6d5f1dcd71268dd4
Alexiuce <alexiuce@qq.com>
2018-03-23 13:23:21 +0800

    add dev-prod yml & @RequestParam

--- KStudy/09SpringBootKotlin/src/main/kotlin/github/alexiuce/HelloController.kt
+++ KStudy/09SpringBootKotlin/src/main/kotlin/github/alexiuce/HelloController.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
  */
 
 @RestController
+@RequestMapping("/hello")
 class HelloController{
 
      @Autowired
@@ -22,10 +24,10 @@ class HelloController{
 //    @Value("\${age}")
 //    var age: Int = 0
 
-    @RequestMapping(value = "/hello", method = [RequestMethod.GET])
-    fun say(): String {
-        
-        return "Hello ${girl.name} , your cupSize is ${girl.cupSize} ,your age is ${girl.age}~"
+    @RequestMapping(value = ["/say"], method = [RequestMethod.POST])
+    fun say(@RequestParam("id",required =false,defaultValue = "0" ) uid: Int): String {
+
+        return "Hello ${girl.name} , your cupSize is ${girl.cupSize} ,your age is ${girl.age}~ your request id is $uid"
     }
 
 }

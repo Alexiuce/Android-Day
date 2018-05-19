0f096a0b1f674651f51bab8b75f2185b0e19343a
Alexiuce <alexiuce@qq.com>
2018-05-19 19:11:10 +0800

    03-day update

--- KStudy/03gradleKotlin/src/main/java/github/alexiuce/kt/HelloKotlin.kt
+++ KStudy/03gradleKotlin/src/main/java/github/alexiuce/kt/HelloKotlin.kt
@@ -5,6 +5,47 @@ package github.alexiuce.kt
  */
 
 fun main(args: Array<String>) {
+
+    /** 基本数据类型1: 布尔  */
+    val isApp: Boolean = true
+    val isIdea: Boolean = false
+    println(isApp)
+
+
+    /** 基本数据类型2: Number: Int, Long ,Float, Double, Short, Byte */
+
+    val num: Int = 8
+    println(num)
+    println(Int.MAX_VALUE)
+    val binnum: Int = 0b1100
+    val hexnum: Int = 0xFF
+    println(binnum)
+    println(hexnum)
+
+    val aNumber: Long = 123
+    println(34L)
+    println(aNumber)
+    println(Long.MAX_VALUE)
+
+    val aFloat: Float = 1.23f
+    println(aFloat)
+    println(Float.NaN)
+
+    val aDouble: Double = 3.14
+    println(aDouble)
+
+    /** Short: 16bit */
+    val aShort: Short = 234
+    println(aShort)
+    println(Short.MAX_VALUE)
+
+    /** Byte: 8bit -128~127 */
+    val aByte: Byte = 111
+    println(Byte.MAX_VALUE)
+    println(Byte.MIN_VALUE)
+
+
+    /** 类 */
     println(Person(21,"ajax"))
 
 
@@ -18,3 +59,4 @@ class HelloKotlin{
 
     }
 }
+

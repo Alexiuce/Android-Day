dc5d48ebfe72f90ba4c1a51bb45fb577c000d2f1
Alexiuce <alexiuce@qq.com>
2018-06-03 17:43:40 +0800

    parames

--- KStudy/03gradleKotlin/src/main/kotlin/github/alexiuce/kt/08-try.kt
+++ KStudy/03gradleKotlin/src/main/kotlin/github/alexiuce/kt/08-try.kt
@@ -22,6 +22,13 @@ package github.alexiuce.kt
 
 * */
 
+// 具名参数 : 调用函数时,传递参数时带上形参的名称
+// 变长参数 : vararg 形参名称    (变长参数可以在方法或者函数的任意位置)
+// 默认参数: 定义函数时,指定形参的值
+
+
+fun my_plus(arg1: Int, arg2:Int): Int = arg1 + arg2
+
 
 
 
@@ -33,6 +40,8 @@ fun main(args: Array<String>) {
         println("args error")
     }
 
-
+    /* 具名参数调用函数 */
+    val a = my_plus(1,arg2 = 3)
+    println(a)
 
 }

d7ddad80160e9c59aa57be65337645cc0a8c1311
Alexiuce <alexiuce@qq.com>
2018-06-15 13:52:46 +0800

    kotlin update

---
+++ KStudy/03gradleKotlin/src/main/kotlin/github/alexiuce/kt/10-string.kt
@@ -0,0 +1,16 @@
+package github.alexiuce.kt
+
+
+
+
+fun main(args: Array<String>) {
+    val place = "浙江省杭州市拱墅区"
+    println(place)
+    val place1 = """
+        浙江省
+        杭州市
+        拱墅区
+    """.trimIndent()
+    println(place1)
+
+}

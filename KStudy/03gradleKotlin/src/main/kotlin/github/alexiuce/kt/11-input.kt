a6ec3341531ff4bdd5623d3d1b59b5684f6b006e
Alexiuce <alexiuce@qq.com>
2018-06-19 14:26:20 +0800

    function update

--- KStudy/03gradleKotlin/src/main/kotlin/github/alexiuce/kt/11-input.kt
+++ KStudy/03gradleKotlin/src/main/kotlin/github/alexiuce/kt/11-input.kt
@@ -2,5 +2,11 @@ package github.alexiuce.kt
 
 fun main(args: Array<String>) {
 
-    println("hell")
+
+    val m = readLine()?.toInt()?: -1    // readLine 从控制台获取用户输入
+    val n = readLine()?.toInt()?: -1
+
+
+    println("$m + $n = ${m + n}")
+
 }

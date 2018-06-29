6f90120a38679157f7073894d270c2ac2ccfaff2
Alexiuce <alexiuce@qq.com>
2018-06-29 11:37:05 +0800

    class demo simple

---
+++ KStudy/04kotlin/src/main/kotlin/github/alexiuce/08-面向对象.kt
@@ -0,0 +1,20 @@
+package github.alexiuce
+
+fun main(args: Array<String>) {
+
+
+    val g = Girl()
+    println(g.name)
+    g.sayHello()
+}
+
+
+class Girl{
+    // 属性
+    var name = "lin"
+    var age = 10
+    // 行为
+    fun sayHello(){
+        println("hello girl")
+    }
+}

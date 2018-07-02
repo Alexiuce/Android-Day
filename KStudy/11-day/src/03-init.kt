3bb8327afb11910b208f4deacf190ca7337b9fee
Alexiuce <alexiuce@qq.com>
2018-07-02 12:36:23 +0800

    class init

---
+++ KStudy/11-day/src/03-init.kt
@@ -0,0 +1,30 @@
+/* 构造方法 */
+
+
+
+
+
+
+class Person1(var name: String, var age: Int){
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+fun main(args: Array<String>) {
+
+    val p1 = Person1("job",20)
+    println(p1.name)
+}

8b775b736eec45041fa3575d4e9a991ed277dc25
Alexiuce <alexiuce@qq.com>
2018-07-13 09:31:56 +0800

    kotlin object up

--- KStudy/11-day/src/08-扩展函数.kt
+++ KStudy/11-day/src/08-扩展函数.kt
@@ -7,6 +7,8 @@ fun main(args: Array<String>) {
 
     val first = "hello"
     println(first.myCheck())
+
+    NetTool.sayH()
 }
 
 
@@ -23,6 +25,16 @@ fun String.myCheck(): Boolean{
 }
 
 /* 单利模式: 饿汉方式(Java常用) , 懒汉方式(Objective-C常用)
- *
-  *
-  * */
+*  1. kotlin 中使用object 代替class 定义单利
+*  2. object中所有的字段都是静态,方式不是静态
+*  3. object 对字段不多时比较合适
+* */
+
+
+
+object NetTool{
+    var name = "jobs"
+    fun sayH(){
+        println(name)
+    }
+}

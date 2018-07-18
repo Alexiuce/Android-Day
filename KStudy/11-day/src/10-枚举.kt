ad8ccd9f2bf3b2759c9513bd9459d0998974c51f
Alexiuce <alexiuce@qq.com>
2018-07-18 08:25:54 +0800

    kotlin enum class

---
+++ KStudy/11-day/src/10-枚举.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+
+
+    println(Week.星期一)
+
+}
+
+
+/* kotlin 枚举
+*
+* enum class 枚举名{
+*   枚举值1,枚举值2
+* }
+*
+* */
+
+
+enum class Week{
+    星期一,星期二,星期三,星期四,星期五,星期六,星期日
+}

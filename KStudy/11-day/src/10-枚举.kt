04324353fe8a41b3d8ccbfb1607e26a2ec6420da
Alexiuce <alexiuce@qq.com>
2018-07-18 08:28:58 +0800

    kotlin enum class advance

--- KStudy/11-day/src/10-枚举.kt
+++ KStudy/11-day/src/10-枚举.kt
@@ -3,6 +3,10 @@ fun main(args: Array<String>) {
 
     println(Week.星期一)
 
+    println(COLOR.RED)
+    println(COLOR.RED.r)
+    
+
 }
 
 
@@ -17,4 +21,10 @@ fun main(args: Array<String>) {
 
 enum class Week{
     星期一,星期二,星期三,星期四,星期五,星期六,星期日
+}
+
+enum class COLOR(var r: Int, var g: Int, var b: Int){
+    RED(255,0,0),
+    GREEN(0,255,0),
+    BLUE(0,0,255)
 }

e15b612aa0a54c34a0c26fa8f11d3b4877c2a781
Alexiuce <alexiuce@qq.com>
2018-07-24 08:32:55 +0800

    kotlin val x = lambda

--- KStudy/11-day/src/16-lambda.kt
+++ KStudy/11-day/src/16-lambda.kt
@@ -5,6 +5,11 @@ fun main(args: Array<String>) {
     val block = {a: Int, b: Int -> a + b}
     val result = block(1,4)
     println(result)
+
+    // 2. 
 }
 
 
+fun haha(a: Int,block:(Int)->Int): Int{
+    return block(a)
+}

637535b5cceb03ea91e8a68ab1eb6356ff1fa85c
Alexiuce <alexiuce@qq.com>
2018-07-30 08:10:14 +0800

    kotlin let func demo

--- KStudy/11-day/src/17-四大函数.kt
+++ KStudy/11-day/src/17-四大函数.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
 
     val list1 = arrayListOf("感冒药","消炎药","驱蚊药","解毒药")
-    /* 1. apply 函数
+    /** 1. apply 函数
      * 任意类型都有apply 函数
      * 参数是一个函数: T.()->Unit (带接收者的函数)
      * this代表调用的对象
@@ -14,6 +14,16 @@ fun main(args: Array<String>) {
     }
 
 
+    /** 2. let函数
+     * 任意类型都有let函数
+     * 参数是是一个函数: 函数参数是调用者本身 (T) -> R 返回值就是lambda表达式的返回值
+     *
+     * */
+    list1.let {
+        it.add("田七")
+    }
+
+    println(list1)
 
 }
 

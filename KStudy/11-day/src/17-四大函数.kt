3b0aff60ec20cfc4628b4e7f657c563999b4158f
Alexiuce <alexiuce@qq.com>
2018-07-30 08:15:47 +0800

    kotlin with func demo

--- KStudy/11-day/src/17-四大函数.kt
+++ KStudy/11-day/src/17-四大函数.kt
@@ -13,7 +13,7 @@ fun main(args: Array<String>) {
         add("感冒片")
     }
 
-
+    println(list1)
     /** 2. let函数
      * 任意类型都有let函数
      * 参数是是一个函数: 函数参数是调用者本身 (T) -> R 返回值就是lambda表达式的返回值
@@ -25,6 +25,18 @@ fun main(args: Array<String>) {
 
     println(list1)
 
+    /** 3. with函数
+     * 是独立函数,可以任意地方调用
+     * 需要两个参数
+     * 第一个参数可以是任意类型
+     * 第二个参数是函数: 并且这个函数的参数是带接收者字面量,接收这就是第一个参数
+     * with的返回值是第二个函数参数的返回值
+     * */
+
+    with(list1){
+        add("当归")
+    }
+    println(list1)
 }
 
 

99ae90649dd66ca8e1956b240022594a83f0658f
Alexiuce <alexiuce@qq.com>
2018-08-02 08:13:28 +0800

    kotlin dsl 1 update

--- KStudy/11-day/src/19-DSL.kt
+++ KStudy/11-day/src/19-DSL.kt
@@ -4,3 +4,18 @@
 * 3. 带接收者的函数字面值
 * 4. 高阶函数(lambda表达式)
 * */
+
+
+
+fun main(args: Array<String>) {
+    val addr = DAddress("深圳","深南大道",23)
+    val person = DPerson("关之琳",30,addr)
+
+
+    
+}
+
+// 地址信息
+class DAddress(var city: String, var street: String, var number: Int)
+// 人物信息
+class DPerson(var name: String, var age: Int, var address: DAddress)

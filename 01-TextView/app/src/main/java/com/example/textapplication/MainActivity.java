e43419cce32526ead0f8df887cb3326a708cad80
秀策 <caijinzhu@dong10.com>
2019-05-14 11:10:30 +0800

    feat(#01): new project

---
+++ 01-TextView/app/src/main/java/com/example/textapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.textapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    public void showTextView(View v){
+        Log.d(TAG, "showTextView: ====");
+    }
+}

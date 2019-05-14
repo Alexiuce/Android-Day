7d568f8d2c63deb350b7abfd994877976a49e33f
秀策 <caijinzhu@dong10.com>
2019-05-14 14:07:39 +0800

    feat(#01): 添加ButtonActivity

--- 01-TextView/app/src/main/java/com/example/textapplication/MainActivity.java
+++ 01-TextView/app/src/main/java/com/example/textapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.textapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showButtonView(View view) {
-        Log.d(TAG, "showButtonView: button view");
+        Intent it = new Intent(MainActivity.this,ButtonActivity.class);
+        startActivity(it);
+
     }
 }

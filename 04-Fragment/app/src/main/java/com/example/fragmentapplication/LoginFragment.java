b3d5012a0d1a95e9b44b4c489eb9dded15d9eba9
秀策 <caijinzhu@dong10.com>
2019-05-28 21:07:50 +0800

    feat(#04 fragment): fragment 事件响应

---
+++ 04-Fragment/app/src/main/java/com/example/fragmentapplication/LoginFragment.java
@@ -0,0 +1,22 @@
+package com.example.fragmentapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import static android.content.ContentValues.TAG;
+
+public class LoginFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login_layout,container,false);
+    }
+
+
+}

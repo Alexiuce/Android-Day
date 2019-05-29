acd9c52ff1c6805c57f2c733a02a728c918c4e73
秀策 <alexiuce@qq.com>
2019-05-29 19:10:05 +0800

    feat(#05): HTTP library

---
+++ 05-Http/httplibrary/src/main/java/com/example/httplibrary/HttpTool.java
@@ -0,0 +1,10 @@
+package com.example.httplibrary;
+
+import android.util.Log;
+
+public class HttpTool {
+    private static final String TAG = "HttpTool";
+    public static void fetchData(){
+        Log.d(TAG, "fetchData: xxx0000");
+    }
+}
